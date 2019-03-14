ae5fbc46c5e3dcc9c74ae16cfbe8ca3f24bd0bb9
ivanmjartan <ivan.mjartan@gooddata.com>
2019-03-14 17:29:07 +0100

    Fix proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,27 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(proxy('/gdc', {
+    app.use(proxy("/gdc", {
         "changeOrigin": true,
         "cookieDomainRewrite": "localhost",
         "secure": false,
-        "target": "https://developer.na.gooddata.com/",
+        "target": "https://developer.na.gooddata.com",
         "headers": {
             "host": "developer.na.gooddata.com",
             "origin": null
+        },
+        "onProxyReq": function (proxyReq, req, res) {
+            proxyReq.setHeader('accept-encoding', 'identity')
         }
     }));
-
-    app.use(proxy('/*.html', {
+    app.use(proxy("/*.html", {
         "changeOrigin": true,
         "secure": false,
-        "target": "https://developer.na.gooddata.com/"
+        "target": "https://developer.na.gooddata.com"
+    }));
+    app.use(proxy("/packages/*.{js,css}", {
+        "changeOrigin": true,
+        "secure": false,
+        "target": "https://developer.na.gooddata.com"
     }));
 };
